db6b729bc57ce31eb8457fbf0769419996e55612
DeathStarGnR <nsbailey2010@gmail.com>
2018-08-19 16:35:54 -0400

    js testing

--- Scripts/default.js
+++ Scripts/default.js
@@ -58,7 +58,7 @@ function toggleCollapse() {
 }
 
 function menuInit() {
-   
+
 var menu = document.getElementById("menu");
 var btns = menu.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
@@ -66,7 +66,7 @@ for (var i = 0; i < btns.length; i++) {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
+    $("#main").load(this.attr('href'));
   });
 }}
-
 
